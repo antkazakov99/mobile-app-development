5e9d510255d6ddd891f0b0b37cc8dbe9f34a5cce
Anton Kazakov <ant.kazakov99@gmail.com>
2020-03-01 00:19:52 +0300

    Сделаны задания 3 и 4 практической работы 3

--- Практическая работа 3/src/Main.kt
+++ Практическая работа 3/src/Main.kt
@@ -1,17 +1,18 @@
 fun main()
 {
-    taskTwo()
 
-//    print("Введите номер задания: ")
-//    val num: String? = readLine()
-//    // Выбор номера задания
-//    if (num != null)
-//    {
-//        when (num.toInt())
-//        {
-//            1 -> taskOne()
-//            2 -> taskTwo()
-//            else -> print("Неправильный номер задания: ")
-//        }
-//    }
+    print("Введите номер задания: ")
+    val num: String? = readLine()
+    // Выбор номера задания
+    if (num != null)
+    {
+        when (num.toIntOrNull())
+        {
+            1 -> taskOne()
+            2 -> taskTwo()
+            3 -> taskThree()
+            4 -> taskFour()
+            null -> print("Неправильный номер задания.")
+        }
+    }
 }
